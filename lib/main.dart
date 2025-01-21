d5de86aa7c80db38178ed64a7fe25e808e2f0a43
LEGION <chchchchch123c@gmail.com>
2025-01-21 15:04:38 +0900

    dart fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled2/presentation/screen/grid_view_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
       home: GridViewScreen(),
     )
   );
